a1455231224ac0a718fb3be5a95e0d8c9038a13c
Terasaki Shun <t.tera1217@gmail.com>
2018-10-06 03:46:50 +0900

    calculate homology group

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,158 @@
 extern crate nalgebra as na;
+extern crate num;
+use num::integer::{gcd, lcm};
 use na::DMatrix;
 type Mat = DMatrix<i64>;
+
+#[derive(Debug, Clone)]
+enum ElementaryOperation {
+    Negate(usize),
+    Switch(usize, usize),
+    Add(usize, i64, usize),
+}
+
+fn elem_row_operate(m: &mut Mat, op: &ElementaryOperation) {
+    match op {
+        ElementaryOperation::Negate(row) => for x in m.row_mut(*row).iter_mut() {
+            *x = -*x;
+        },
+        ElementaryOperation::Switch(row1, row2) => {
+            m.swap_rows(*row1, *row2);
+        }
+        ElementaryOperation::Add(row1, c, row2) => {
+            let cols = m.ncols();
+            for i in 0..cols {
+                m[(*row2, i)] = m[(*row2, i)] + *c * m[(*row1, i)];
+            }
+        }
+    }
+}
+
+fn elem_col_operate(m: &mut Mat, op: &ElementaryOperation) {
+    match op {
+        ElementaryOperation::Negate(col) => for x in m.column_mut(*col).iter_mut() {
+            *x = -*x;
+        },
+        ElementaryOperation::Switch(col1, col2) => {
+            m.swap_columns(*col1, *col2);
+        }
+        ElementaryOperation::Add(col1, c, col2) => {
+            let rows = m.nrows();
+            for j in 0..rows {
+                m[(j, *col2)] = m[(j, *col2)] + *c * m[(j, *col1)];
+            }
+        }
+    }
+}
+
+fn row_elimination(m: &mut Mat, idx: usize) -> bool {
+    let rows = m.nrows();
+    let mut minval = 1000;
+    let mut mini = 0;
+    for i in idx..rows {
+        let absval = m[(i, idx)].abs();
+        if 0 < absval && absval < minval {
+            minval = absval;
+            mini = i;
+        }
+    }
+    if minval == 1000 {
+        return false;
+    }
+    elem_row_operate(m, &ElementaryOperation::Switch(idx, mini));
+    if m[(idx, idx)] < 0 {
+        elem_row_operate(m, &ElementaryOperation::Negate(idx));
+    }
+    let mut nz_idx = idx;
+    for i in (idx + 1)..rows {
+        let q = m[(i, idx)] / m[(idx, idx)];
+        elem_row_operate(m, &ElementaryOperation::Add(idx, -q, i));
+        if m[(i, idx)] != 0 {
+            nz_idx = i;
+        }
+    }
+    nz_idx != idx
+}
+fn col_elimination(m: &mut Mat, idx: usize) -> bool {
+    let cols = m.ncols();
+    let mut minval = 1000;
+    let mut minj = 0;
+    for j in idx..cols {
+        let absval = m[(idx, j)].abs();
+        if 0 < absval && absval < minval {
+            minval = absval;
+            minj = j;
+        }
+    }
+    if minval == 1000 {
+        return false;
+    }
+    elem_col_operate(m, &ElementaryOperation::Switch(idx, minj));
+    if m[(idx, idx)] < 0 {
+        elem_col_operate(m, &ElementaryOperation::Negate(idx));
+    }
+    let mut nz_idx = idx;
+    for j in (idx + 1)..cols {
+        let q = m[(idx, j)] / m[(idx, idx)];
+        elem_col_operate(m, &ElementaryOperation::Add(idx, -q, j));
+        if m[(idx, j)] != 0 {
+            nz_idx = j;
+        }
+    }
+    nz_idx != idx
+}
+fn diagonalize(m: &Mat) -> (Mat, usize) {
+    let mut m = m.clone();
+    let (rows, cols) = m.shape();
+    let dsize = if rows < cols { rows } else { cols };
+    for idx in 0..dsize {
+        while row_elimination(&mut m, idx) {}
+        while col_elimination(&mut m, idx) {}
+    }
+    let mut nzlen = 0;
+    for idx in 0..dsize {
+        if m[(idx, idx)].abs() > 0 {
+            nzlen = nzlen + 1;
+        }
+    }
+    (m, nzlen)
+}
+
+fn smith(m: &Mat) -> Vec<i64> {
+    let (d, nzlen) = diagonalize(&m);
+    let mut v = vec![0; nzlen];
+    for i in 0..nzlen {
+        v[i] = d[(i, i)];
+    }
+    convert_divisible_seq(&v)
+}
+
+fn is_divisible(v: &Vec<i64>) -> bool {
+    let len = v.len();
+    for i in 0..len - 1 {
+        if v[i + 1] % v[i] != 0 {
+            return false;
+        }
+    }
+    true
+}
+
+fn convert_divisible_seq(v: &Vec<i64>) -> Vec<i64> {
+    let mut v = v.clone();
+    let len = v.len();
+    while !is_divisible(&v) {
+        v.sort();
+        for i in 0..(len - 1) {
+            for j in (i + 1)..len {
+                let (g, l) = (gcd(v[i], v[j]), lcm(v[i], v[j]));
+                v[i] = g;
+                v[j] = l;
+            }
+        }
+    }
+    v
+}
+
 type Simplex = Vec<usize>;
 type Chain = Vec<Vec<Simplex>>;
 
@@ -45,6 +197,37 @@ impl SimplicialComplex {
         }
         mat
     }
+    fn homology_group(&self, q: usize) -> FinitelyGeneratedModule {
+        let mat1 = self.boundary_mat(q);
+        let mat2 = self.boundary_mat(q + 1);
+        let (v1, v2) = (smith(&mat1), smith(&mat2));
+        let rank = mat2.nrows() - v1.len() - v2.len();
+        let mut torsion: Vec<i64> = Vec::new();
+        for a in v2 {
+            if a != 1 {
+                torsion.push(a);
+            }
+        }
+        FinitelyGeneratedModule {
+            torsion: torsion,
+            rank: rank,
+        }
+    }
+}
+
+struct FinitelyGeneratedModule {
+    torsion: Vec<i64>,
+    rank: usize,
+}
+
+impl FinitelyGeneratedModule {
+    fn to_string(&self) -> String {
+        let mut s = String::new();
+        for a in &self.torsion {
+            s = s + &format!("Z/{}Z⊕", a);
+        }
+        s + &format!("Z^{}", self.rank)
+    }
 }
 
 fn main() {
@@ -62,10 +245,5 @@ fn main() {
     let c0 = vec![vec![0], vec![1], vec![2], vec![3], vec![4]];
     let c = vec![c0, c1, c2];
     let sc = SimplicialComplex::from_chain(&c);
-    let mat1 = sc.boundary_mat(0);
-    let mat2 = sc.boundary_mat(1);
-    println!("d1 = {}", mat1);
-    println!("d2 = {}", mat2);
-    let mat3 = mat1 * mat2;
-    println!("d1 * d2 = {}", mat3);
+    println!("H_0 = {}", sc.homology_group(0).to_string());
 }
